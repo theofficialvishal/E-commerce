19b64505d3e16c956b3fcf654116187a0ae8f910
theofficialvishal <134837701+theofficialvishal@users.noreply.github.com>
2026-05-18 19:37:34 +0530

    Add cart routes and controllers, fix subdocument reference logic

---
+++ Backend/routes/cart.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  addToCart,
+  getCartProducts,
+  removeAllFromCart,
+  updateQuantity,
+} from "../controllers/cart.controller.js";
+import { protectedRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", protectedRoute, getCartProducts);
+router.post("/", protectedRoute, addToCart);
+router.delete("/", protectedRoute, removeAllFromCart);
+router.put("/:id", protectedRoute, updateQuantity);
+
+export default router;
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -15,6 +16,7 @@ app.use(cookieParser()); //allows server to parse cookies from request headers
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
